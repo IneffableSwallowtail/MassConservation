163d2a8bd16cf05180b32e3e410e5fb4c6b6ab75
A.J. Miller <ajmiller1206@gmail.com>
2022-10-22 20:59:19 -0400

    added notes for development

--- MassConservation/Program.cs
+++ MassConservation/Program.cs
@@ -25,3 +25,12 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+/* Research local storage, or how to keep data safe for users?
+ * Fill out MySqlDatabase
+ * Research informational links to add to database
+ * Add bill API
+ * -Consider adding numbers to local and state senators
+ * Look for API for ecoloical organizations
+ * Add recycling API
+ * Add functionality to make your own database
+ */
