e557239a46b1a90c6ad370ca8d830ac2de83e6c1
A.J. Miller <ajmiller1206@gmail.com>
2022-10-26 15:44:06 -0400

    'centered' text vertically in link squares

---
+++ MassConservation/Controllers/BillSearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MassConservation.Controllers
+{
+    public class BillSearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
