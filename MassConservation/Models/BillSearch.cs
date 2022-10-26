6246a32e3fa1e0733e3864cc068b7793f81a2996
A.J. Miller <ajmiller1206@gmail.com>
2022-10-26 12:48:18 -0400

    added BillSearch model

---
+++ MassConservation/Models/BillSearch.cs
@@ -0,0 +1,10 @@
+﻿namespace MassConservation.Models
+{
+    public class BillSearch
+    {
+        public BillSearch() { }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+    }
+}
